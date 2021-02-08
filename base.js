a3a64b7463f9c5bee070554c6e2d573c414bbf2a
LeviathanProgramming <connerow1115@gmail.com>
2021-02-08 04:10:22 -0600

    Create base.js

---
+++ base.js
@@ -0,0 +1,24 @@
+const $ = (prop) => document.querySelector(prop);
+const $$ = (prop) => document.querySelectorAll(prop);
+const $st = (prop) => $(prop).style;
+function animateElement(element, animation){
+  $(element).style.animation = animation;
+  $(element).style.animationFillMode = "forwards";
+}
+function cssVar(Var, Val){
+  $(":root").style.setProperty(Var, Val);
+}
+function setStats(){
+  cssVar("--full-height",window.innerHeight+"px");
+  cssVar("--full-width",window.innerWidth+"px");
+}
+window.onload = setStats;
+window.onresize = setStats;
+function closeNav(){
+  $st("#nav").transition = "0.5s";
+  $st("#nav").left = "-202px";
+}
+function openNav(){
+  $st("#nav").transition = "0.5s";
+  $st("#nav").left = "0px";
+}
